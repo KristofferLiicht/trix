a2397b1eeb2ec6d1ab01c321b3fa5af7ed96a630
KristofferLiicht <foffer9@gmail.com>
2015-09-02 01:07:36 +0200

    created little addTrick page for inserting documents in collection

--- client/routes.jsx
+++ client/routes.jsx
@@ -7,6 +7,7 @@ const {
 const routes = (
   <Route name="root" path="/" handler={App}>
     <Route name="tricks" path="/tricks/:trickName" handler={TrickPage}/>
+    <Route name="addTrick" path= "/addTrick" handler={AddTrickForm}/>
     <DefaultRoute handler={LandingPage} />
     <NotFoundRoute handler={AppNotFound} />
   </Route>
@@ -29,41 +30,4 @@ Meteor.startup(function () {
     //<Route name="join" path="/join" handler={AuthJoinPage} />
     //<Route name="signin" path="/signin" handler={AuthSignInPage} />
 
-var appRoutes = FlowRouter.group({
-  subscriptions: function() {
-    this.register('userData', Meteor.subscribe('userData'));
-    this.register('graphs', Meteor.subscribe('graphs'));
-  },
-  triggersEnter: [
-    function(context, redirect) {
-      if (!Meteor.user()) {
-        redirect('/');
-      }
-    }
-  ]
-});
-
-memberRoutes.route('/feed', {
-  action: function() {
-    ReactLayout.render(DashboardLayout, {
-      content: <Feed/>
-    });
-  }
-});
-
-memberRoutes.route('/publish', {
-  action: function() {
-    ReactLayout.render(DashboardLayout, {
-      content: <Publish/>
-    });
-  }
-});
-
-memberRoutes.route('/settings', {
-  action: function() {
-    ReactLayout.render(DashboardLayout, {
-      content: <SettingsPage/>
-    });
-  }
-});
 
