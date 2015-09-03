60c5946cdbe5719dad39d609403ce3aaac498fd2
KristofferLiicht <foffer9@gmail.com>
2015-09-03 13:45:33 +0200

    created more personalized facebook button

--- client/components/tricks/TrickPage.jsx
+++ client/components/tricks/TrickPage.jsx
@@ -37,27 +37,50 @@ TrickPage = React.createClass({
     });
   },
 
+  _handleOnClickFace() {
+    const shareUrl = "http://localhost:3000" + this.getPathname();
+    
+    FB.ui({
+          method: 'feed',
+          link: shareUrl, 
+          picture: "http://www.ocdtypes.com/graphics/order_lighters.jpg",
+          name: "Light Trick!",
+          description: "Awesome trick man! Shine briiight like liiight"
+        }, function(response){
+            console.log(response);
+        }
+      );
+
+    window.open("https://www.facebook.com/dialog/share?app_id=521513011337668&display=popup&href=" + shareUrl + "&redirect_uri=" + shareUrl);
+  },
 
 
   renderTrickPageHeader(){
+    const shareUrlFace = this.getPathname();
+    const title = "Learn " + this.data.trick.trickName;
+    const onClickFace = "window.open('https://www.facebook.com/dialog/share?app_id=521513011337668&display=popup&href=" + shareUrlFace + "&redirect_uri=" + shareUrlFace +"')"
+   
+
     return(
           <div className="row">
               <div className="ui ten wide column">
                 <h1> {this.data.trick.trickName} </h1>
               </div>
               <div className="ui six wide column">
-                <div className="fb-like" 
-                    data-href="http://www.your-domain.com/your-page.html" 
-                    data-layout="button" 
-                    data-action="like" 
-                    data-show-faces="true">
-                </div>
-                <div className="fb-share-button" 
-                     data-href="https://developers.facebook.com/docs/plugins/" 
-                     data-layout="button">
-                </div>              
+                <div className="ui menu">
+                  <a className="fa fa-facebook share-base share-facebook" onClick={this._handleOnClickFace}>
+                    <div className="item">
+                      Share on Facebook!
+                    </div>
+                  </a>
+                  <div className="item">
+                    <div className="ui button">Log-in</div>
+                  </div>
+                </div>                  
               </div>
           </div>
+
+
     );
   },
 
@@ -90,5 +113,19 @@ TrickPage = React.createClass({
     if (! trick) {
       return <AppLoading />;
     }
+
+    <div className="fb-like" 
+        data-href="http://www.your-domain.com/your-page.html" 
+        data-layout="button" 
+        data-action="like" 
+        data-show-faces="true">
+    </div>
+    <div className="fb-share-button" 
+         data-href="https://developers.facebook.com/docs/plugins/" 
+         data-layout="button">_
+    </div>   
+
+
+     onclick="require("Popup").open("\/sharer.php?u=[URL]&display=popup&ref=plugin&src=share_button&app_id=150339008443796", 670, 340);"
  */
 
